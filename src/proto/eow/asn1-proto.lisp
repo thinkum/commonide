4af04dd2f13c11da944f923f71f350d78e8e6ce6
Sean Champ <spchamp@users.noreply.github.com>
2019-07-01 09:51:48 -0700

    Add initial support for identifier construction; define a principal declarations model
    - Begin support for locally specified *FEATURES* element :ASN.1-UNICODE
    
    - Define an initial outline for intermediate representation of ASN.1
      sytnactic elements as ASN.1-DECLARATION; this may serve to provide a
      manner of a type signatures model for interpreted ASN.1 expressions
      and locally constructed ASN.1 expressions, in subsequent extension.
    
    - Define IDENTIFIER-P, SIMPLIFY-IDENTIFIER functions, reusing the
      portable LTP/COMMON:DEFUN* macro in the functions' definitions.
    
    - Begin adding annotations with reference to unambiguous definitions in
      ITU-T Technical Recommendations, those providing a standards framework
      for ASN.1 itself.
    
    - TBD: Parser-Token model for local stream-oriented processing of ASN.1
      source expressions, independent of external tools for ASN.1.

--- src/proto/eow/asn1-proto.lisp
+++ src/proto/eow/asn1-proto.lisp
@@ -20,6 +20,10 @@
 
 (eval-when (:compile-toplevel :execute)
 
+#+sb-unicode
+(pushnew :asn.1-unicode *features* :test #'eq)
+;; TBD Other
+
 (defmacro mkv (contents &optional (element-type t))
   (with-symbols (%c)
     `(let ((,%c ,contents))
@@ -62,3 +66,158 @@
 ;; - NB 'Any Type' declations in ASN.1
 ;; - NB "Strong typing" with ASN.1 in applications
 ;; - NB Structure and Modularization in ASN.1 Protocol Definitions
+
+(defstruct (asn.1-declaration
+             (:conc-name declaration-)
+             (:constructor))
+  (name
+   (mk-lf "Unnamed Declaration")
+   :type simple-string
+   :read-only t))
+
+(defstruct (asn.1-type-declaration
+             (:include asn.1-declaration)
+             (:constructor))
+  ;; NB ASN.1 "Any" type in abstract structural declarations
+
+  ;; NB: Type tagging for disambiguation of generalized union values -
+  ;; refer to annotation, subsq.
+  )
+
+;; TBD: Signature syntax for ASN.1 generalized union signatures
+;; i.e SEQUENCE, SET, CHOICE
+;; ... and ENUMERATION? as relevant in module-specific tagging
+;; behaviors, described to an extent at p. 113 (PDF p. 141) of
+;; [Dubuisson]
+;;
+;; - Note that each generalized union memebr must have a tag stored with
+;; it, complimentary to the tag - in the same sense - as assigned to the
+;; type of the member. Refer, perhaps, to [Dubuisson] PDF pp. 234-246
+
+;; NB: If this system is implemented for interoperation with an ASN.1
+;; interpreter e.g in C, this type signature model may be revised for
+;; further support of the particular interpreter tool.
+
+
+(defstruct (asn.1-structure-declaration
+             ;; TBD: Too simplified (??)
+             (:include asn.1-declaration)
+             (:constructor))
+  )
+
+
+;; -- Prototypes towards a parser semantics
+
+;; (defvar +asn.1-readtable+ ...)
+
+;; (defvar *case-fold-asn.1-identifiers* ??)
+;; ^ TBD: Compiler macro for IDENTIFIER-P dispatching on that value,
+;; during evaluation
+
+(defun* identifier-p (str)
+  (declare (type string str)
+           (values boolean (or character null) &optional))
+  (let ((len (length str))
+        (in-dash))
+    (declare (type array-index len))
+    (cond
+      ((zerop len) (values nil nil))
+      (t
+       (let ((c0 (char str 0))
+             (c-last (char str (1- len))))
+         (cond
+           ((not (and (alpha-char-p c0)
+                      ;; TBD case-folding per runtime config
+                      (lower-case-p c0)))
+            (values nil c0))
+           ((char= c-last #\-)
+            (values nil c-last))
+           (t
+            (do ((n 1 (1+ n)))
+                ((= n len) (values t nil))
+              (declare (type array-index n))
+              (let ((c (aref str n)))
+                (cond
+                  ((alphanumericp c)
+                   (setq in-dash nil))
+                  ((or (char= c #\-)
+                       ;; NB: #\NON-BREAKING_HYPHEN
+                       #+:asn.1-unicode
+                       (char= c (mk-lf (code-char #x2011))))
+                   (if in-dash (return (values nil c))
+                       (setq in-dash t)))
+                  (t (return (values nil c))
+                     )))))))))))
+
+;; (identifier-p "frob")
+
+;; (identifier-p "Frob")
+
+;; (identifier-p "-")
+
+;; (identifier-p "frob-b")
+
+;; (identifier-p "frob--b")
+
+;; (identifier-p "frob-b-")
+
+
+;; ^ cf. ASN OID symbolic elements
+;; Ref: ITU-T Recommendation X.680 (08/2015) subclause 12.3,
+;; "Identifiers," and clause 11, "The ASN.1 Character Set"
+;;
+;; NB: A subset of/ Unicode characters is supported for identifier
+;; tokens in ASN.1, per X.680 (08/2015) subclauses 11.1 and 12.3
+;; generally: Letter characters, Digit characters, and "Hyphens" broadly
+;; (see susbq) such that may be understood as denoting complimetary
+;; subsets of code points in UCS. As such, this software system will
+;; rely on the implementation's support for character predicate
+;; functions onto the respective UCS code points.
+;;
+;; NB: ITU-T Recommendation X.680 (08/2015) permits the usage of a
+;; non-breaking hyphen character in identifier tokens, but specifies -
+;; in effect - that it is to be interpreted as representing a hyphen-
+;; minus character.
+;;
+;; NB: LTP/COMMON:SIMPLIFY-STRING
+
+
+(defun* simplify-identifier (str)
+  (declare (type string str)
+           (values simple-string &optional))
+  (let* ((len (length str))
+         (buf (make-array len
+                          :element-type 'character
+                          :initial-contents str))
+         non-base-str)
+    (declare (type array-index len)
+             (type simple-string buf))
+    (labels ((parse ()
+               (if non-base-str (values buf)
+                   (coerce buf 'simple-base-string))))
+      (dotimes (n len (parse))
+        (let ((c (schar buf n)))
+         (cond
+          ;; NB: #\NON-BREAKING_HYPHEN interpreted as #\-
+           #+:asn.1-unicode
+           ((char= c (mk-lf (code-char #x2011)))
+            (setf (schar buf n) #\-))
+           ((typep c 'base-char)) ;; no-op
+           (t
+            (setq non-base-str t))))))))
+
+
+;; (typep (simplify-identifier "frob") 'simple-base-string)
+;; =>T
+
+#+NIL
+(eval-when ()
+  #+asn.1-unicode
+  (let ((s  (make-string 3 :initial-element #\a)))
+    ;; NB: #\NON-BREAKING_HYPHEN
+    (setf (schar s 1) (code-char #x2011))
+    (setq s (simplify-identifier s))
+    (values s (aref s 1)))
+  ;; => "a-a", #\-
+
+)
